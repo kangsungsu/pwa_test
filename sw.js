19cf308880659bc718f4139a73c0e28bcae96800
KangSungSu <tmdtn0312@naver.com>
2022-04-28 18:48:24 +0900

    v2.0

--- sw.js
+++ sw.js
@@ -1,26 +1,7 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
+self.addEventListener('fetch', function (event) {
+  //   여기에 코드를 넣지 않아도 설치는 가능하다.   
+  console.log("fetch!!",event.request.url)
+  
+})
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+const offlineFallbackPage = "offline.html"
