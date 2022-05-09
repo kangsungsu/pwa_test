8cd6bb435aa1848d2fa8cea6689427ff41a603b0
KangSungSu <tmdtn0312@naver.com>
2022-05-09 18:09:30 +0900

    pw

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+self.caches;
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          'phones/galaxy_a12_1.jpg'
+        ]
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function (event) {
   //   여기에 코드를 넣지 않아도 설치는 가능하다.   
   console.log("fetch!!",event.request.url)
